2b59a2267f2580ec305cd813b3cde845f2f6a6e6
mohammadtahah38-rgb <mohammadtahah38@gmail.com>
2025-08-14 02:49:32 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,18 @@
 import os
 from telebot import TeleBot
 
-TOKEN = os.getenv("8301827690:AAG62x2naz4okPNVf9-Z6xmXZAxPN4F7B9U")
+# گرفتن توکن از متغیر محیطی Render
+TOKEN = os.getenv("BOT_TOKEN")
 bot = TeleBot(TOKEN)
 
+# دستور شروع ربات
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "سلام! ربات ستاره فعال شد 🌟")
+
+# دستور تست ستاره دادن (مثال ساده)
+@bot.message_handler(commands=['star'])
+def star(message):
+    bot.reply_to(message, f"{message.from_user.first_name} یک ستاره گرفت ⭐")
 
 bot.polling()
