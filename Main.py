27f4e828137bc38c9bc547d2b8447fcfe6c0a046
mohammadtahah38-rgb <mohammadtahah38@gmail.com>
2025-08-14 02:15:51 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,11 @@
+import os
+from telebot import TeleBot
 
+TOKEN = os.getenv("BOT_TOKEN")
+bot = TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام! ربات ستاره فعال شد 🌟")
+
+bot.polling()
