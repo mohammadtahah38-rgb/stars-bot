64970187c3b1f607808334386563a4b5caf8f8bd
mohammadtahah38-rgb <mohammadtahah38@gmail.com>
2025-08-14 02:19:38 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import os
 from telebot import TeleBot
 
-TOKEN = os.getenv("BOT_TOKEN")
+TOKEN = os.getenv("8301827690:AAG62x2naz4okPNVf9-Z6xmXZAxPN4F7B9U")
 bot = TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
